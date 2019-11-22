ef293ddf32eb8e8471bc223647d760de9286d57a
Aleksandr Sladkov <ASladkov@alfabank.ru>
2019-11-22 12:31:50 +0300

    feat(*): make task 1

--- src/task1.js
+++ src/task1.js
@@ -1,1 +1,12 @@
-console.log(1);
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
+rl.on('line', function(line) {
+    const reverseLine = line.split('').reverse().join('');
+    console.log(`${reverseLine}\n`);
+});
