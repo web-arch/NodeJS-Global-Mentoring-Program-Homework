e783d6dd22c16a7221b8a96f1be696645562c49f
Aleksandr_Sladkov <asladkov@alfabank.ru>
2021-11-28 21:41:51 +0300

    feat(*): additional task for module 1

--- module1/task2/main.js
+++ module1/task2/main.js
@@ -1,23 +1,26 @@
 import fs from 'fs'; 
 import csv from 'csvtojson';
-import { pipeline } from 'stream';
+import { PassThrough } from 'stream';
+
+const writeInDB = (data) => {
+    return new Promise((resolve) => {
+        setTimeout(() => { resolve(data) }, 1000) 
+    })
+}
 
 const csvFile = './task2/csv/node_mentoring_t1_2_input_example.csv';
 const txtFile = './task2/result/result.txt';
 
 const readStream = fs.createReadStream(csvFile);
-const writeStream = fs.createWriteStream(txtFile, { flag: 'a' });
+const parseCsvStream = readStream.pipe(csv({ ignoreColumns: /amount/ig }));
 
-pipeline(
-    readStream,
-    csv({ ignoreColumns: /amount/ig }),
-    writeStream,
-    (err) => {
-        if(err) {
-            console.error(err);
-        } else {
-            console.log('Success')
-        }
-        
-    }
-);
+const writeInBDStream = new PassThrough();
+
+writeInBDStream.on('data', async function(data) {
+    await writeInDB(data);
+})
+
+const writeToFileStream = fs.createWriteStream(txtFile, { flag: 'a' });
+
+parseCsvStream.pipe(writeInBDStream);
+parseCsvStream.pipe(writeToFileStream);
